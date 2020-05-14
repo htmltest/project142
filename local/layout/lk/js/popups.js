3872b77e83f107754ec9602480baf5f41eb26ac5
htmltest <struev.vs@gmail.com>
2020-05-14 11:49:37 +0300

    selects

--- local/layout/lk/js/popups.js
+++ local/layout/lk/js/popups.js
@@ -26,4 +26,31 @@ $(document).ready(function() {
         });
     }, 100);
 
+    $('.form-select select').each(function() {
+        var curSelect = $(this);
+        var options = {
+            minimumResultsForSearch: 20
+        }
+
+        curSelect.select2(options);
+
+        if (curSelect.find('option:selected').legnth > 0 || curSelect.find('option').legnth == 1 || curSelect.find('option:first').html() != '') {
+            curSelect.trigger({type: 'select2:select'})
+        }
+    });
+
+    $('.form-select-ajax select').each(function() {
+        var curSelect = $(this);
+        var options = {
+            ajax: {
+                url: curSelect.parent().attr('data-link'),
+                dataType: 'json'
+            },
+            minimumInputLength: 3,
+            placeholder: curSelect.parent().attr('data-placeholder')
+        }
+
+        curSelect.select2(options);
+    });
+
 });
